88b07d41994eecc1d354949bc02c2114751158af
Soleil <wilsonx949@gmail.com>
2022-07-10 16:49:12 -0700

    Created UI skeleton

---
+++ deck_builder.py
@@ -0,0 +1,39 @@
+
+
+class DeckBuilder:
+    '''
+    UI for deck builder
+    '''
+
+    def __init__(self):
+        self.deck_list = []
+
+    def search_card(self):
+        pass
+    
+    def load_deck(self):
+        pass
+    
+    def add_deck(self):
+        pass
+
+    def delete_deck(self):
+        pass
+    
+    def save_deck(self):
+        pass
+    
+    def rename_deck(self):
+        pass
+    
+    def run(self):
+        while(True):
+            prompt = 'Select Deck \n' \
+                'New Deck \n' \
+                'Load Deck \n' \
+                'New Deck \n' \
+                'Delete Deck \n' \
+                'Rename Deck \n' \
+                'Exit \n' \
+                'Select an Option: '
+    
