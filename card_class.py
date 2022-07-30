a0f4fa5a159e19442e0dbada8650e2c4e95ed8a2
Soleil <wilsonx949@gmail.com>
2022-07-30 15:59:25 -0700

    Added the extra attributes to the monster class to match the API

--- card_class.py
+++ card_class.py
@@ -15,9 +15,15 @@ class Card:
 class Monster(Card):
     '''
     Monster subclass
+    type refers to:
+    Effect, Flip Effect, Flip Tuner Effect, Gemini, Normal, Normal Tuner,
+    Pendulum Effect, Pendulum Flip Effect, Pendulum Normal, Pendulum Tuner Effect, Ritual Effect,
+    Ritual, Spirit, Toon, Tuner, Union Effect
+    Fusion, Link, Pendulum Effect Fusion, Synchro, Synchro Pendulum Effect,
+    Synchro Tuner, XYZ, XYZ Pendulum
     '''
 
-    def __init__(self, id, name, description, level, attribute, race, type, atk_pts, def_pts):
+    def __init__(self, id, name, description, level, attribute, race, type, atk_pts, def_pts, scale, linkval, linkmarkers):
         super().__init__(id, name, description)
         self.level = level
         self.attribute = attribute
@@ -25,9 +31,12 @@ class Monster(Card):
         self.type = type
         self.atk_pts = atk_pts
         self.def_pts = def_pts
+        self.scale = scale
+        self.linkval = linkval
+        self.linkmarkers = linkmarkers
 
     def __str__(self):
-        return f'{self.name}, {self.level}, {self.atk_pts}, {self.def_pts}'
+        return f'{self.name}, {self.level}, {self.atk_pts}, {self.def_pts} \n{self.description}'
 
 class Spell(Card):
     '''
@@ -40,7 +49,7 @@ class Spell(Card):
         self.type = type
 
     def __str__(self):
-        return f'{self.name}, {self.type}'
+        return f'{self.name}, {self.type} \n{self.description}'
 
 class Trap(Card):
     '''
@@ -53,4 +62,13 @@ class Trap(Card):
         self.type = type
 
     def __str__(self):
-        return f'{self.name}, {self.type}'
+        return f'{self.name}, {self.type} \n{self.description}'
+
+
+mysticmine = Spell(69, 'mystic mine', 'floodgate', 'Field')
+busterblader = Monster(332, 'buster blader', 'kills dragons', 7, 'EARTH', 'Warrior', 'Effect Monster', 2600, 2300, None, None, None)
+imperialorder = Trap(222, 'imperial order', 'stops spells', 'Countinuous')
+
+print(mysticmine)
+print(busterblader)
+print(imperialorder)
